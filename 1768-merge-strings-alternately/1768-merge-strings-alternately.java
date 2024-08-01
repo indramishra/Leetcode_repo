12a1f0d87f0f1b4235f8ccfa8e68871c650de354
indramishra <9indramishra@gmail.com>
2024-08-01 22:48:51 +0530

    Time: 1 ms (86.32%), Space: 41.8 MB (40.04%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder result = new StringBuilder();
+        int maxSize = Math.max(word1.length(), word2.length());
+        for(int i = 0; i < maxSize ; i++) {
+            if(i < word1.length()) {
+                result.append(word1.charAt(i));
+            }
+            if(i < word2.length()) {
+                result.append(word2.charAt(i));
+            }
+        }
+        return result.toString();
+    }    
+}
