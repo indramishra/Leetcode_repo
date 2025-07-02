d4910617fcb12a2af41eb9333f5be69f953146ee
indramishra <9indramishra@gmail.com>
2025-07-02 11:32:17 +0530

    Time: 0 ms (100%), Space: 43.5 MB (10.72%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -5,6 +5,9 @@ class Solution {
         int high = nums.length - 1;
         while(low <= high) {
             int mid = (high + low)/2;
+            if(target == nums[mid]) {
+                return mid;
+            }
             if(target <= nums[mid]) {
                 index = mid;
                 high = mid-1;
